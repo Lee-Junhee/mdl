d7b4b22a5fd0127151c23eba2f0c5ef14e4db5a4
Lee-Junhee <junhee.lee002@gmail.com>
2020-04-30 09:19:54 -0400

    filled out script.py

--- script.py
+++ script.py
@@ -1,4 +1,5 @@
 import mdl
+import math
 from display import *
 from matrix import *
 from draw import *
@@ -49,3 +50,74 @@ def run(filename):
     print(symbols)
     for command in commands:
         print(command)
+        args = command['args']
+        if command['op'] == 'sphere':
+            polygons = []
+            add_sphere(polygons, args[0], args[1], args[2],
+                       args[3], step_3d)
+            matrix_mult(stack[-1], polygons)
+            draw_polygons(polygons, screen, zbuffer, view, ambient, light, symbols, command['constants'])
+        elif command['op'] == 'torus':
+            polygons = []
+            add_torus(polygons, args[0], args[1], args[2],
+                      args[3], args[4], step_3d)
+            matrix_mult(stack[-1], polygons)
+            draw_polygons(polygons, screen, zbuffer, view, ambient, light, symbols, command['constants'])
+        elif command['op'] == 'box':
+            polygons = []
+            add_box(polygons,
+                    args[0], args[1], args[2],
+                    args[3], args[4], args[5])
+            matrix_mult(stack[-1], polygons)
+            draw_polygons(polygons, screen, zbuffer, view, ambient, light, symbols, command['constants'])
+        elif command['op'] == 'circle':
+            edges = []
+            add_circle(edges,
+                       args[0], args[1], args[2],
+                       args[3], step_3d)
+            matrix_mult(stack[-1], edges)
+            draw_lines(edges, screen, zbuffer, color)
+        elif command['op'] in ['hermite', 'bezier']:
+            edges = []
+            add_curve(edges,
+                      args[0], args[1],
+                      args[2], args[3],
+                      args[4], args[5],
+                      args[6], args[7],
+                      step, command['op'])
+            matrix_mult(stack[-1], edges)
+            draw_lines(edges, screen, zbuffer, color)
+        elif command['op'] == 'line':
+            edges = []
+            add_edge(edges,
+                     args[0], args[1], args[2],
+                     args[3], args[4], args[5])
+            matrix_mult(stack[-1], edges)
+            draw_lines(edges, screen, zbuffer, color)
+        elif command['op'] == 'scale':
+            t = make_scale(args[0], args[1], args[2])
+            matrix_mult(stack[-1], t)
+            stack[-1] = [x[:] for x in t]
+        elif command['op'] == 'move':
+            t = make_translate(args[0], args[1], args[2])
+            matrix_mult(stack[-1], t)
+            stack[-1] = [x[:] for x in t]
+        elif command['op'] == 'rotate':
+            theta = args[1] * (math.pi / 180)
+            if args[0] == 'x':
+                t = make_rotX(theta)
+            elif args[0] == 'y':
+                t = make_rotY(theta)
+            else:
+                t = make_rotZ(theta)
+            matrix_mult(stack[-1], t)
+            stack[-1] = [x[:] for x in t]
+        elif command['op'] == 'push':
+            systems.append([x[:] for x in stack[-1]])
+        elif command['op'] == 'pop':
+            stack.pop()
+        elif command['op'] in ['display', 'save']:
+            if command['op'] == 'display':
+                display(screen)
+            else:
+                save_extension(screen, args[0])
